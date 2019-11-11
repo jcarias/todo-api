b9c44396a9297d0555b0d9846acaf928f5ad5304
João Carias <nb22771@novabase.pt>
2019-11-11 15:05:57 +0000

    Added CORS support

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import TodoController from "../todosControllers/Todos";
+import cors from "cors";
 
 const router = express.Router();
 
 const ToDosEndPoint = "/api/v1/todos";
-router.get(`${ToDosEndPoint}`, TodoController.getAllToDos);
+router.get(`${ToDosEndPoint}`, cors(), TodoController.getAllToDos);
 router.get(`${ToDosEndPoint}/:id`, TodoController.getTodo);
 router.post(`${ToDosEndPoint}`, TodoController.createTodo);
 router.put(`${ToDosEndPoint}/:id`, TodoController.updateTodo);
